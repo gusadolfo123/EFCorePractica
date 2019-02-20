04ec1299f4c68caefa119ad57649d2bb2f03bc6d
gusadolfo123 <gusadolfo123@hotmail.com>
2019-02-19 19:58:09 -0500

    asd

---
+++ EFdNorthWind.Web/Config/LocalizationConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFdNorthWind.Web.Config
+{
+    public class LocalizationConfig
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Resources" );
+        }
+    }
+}
